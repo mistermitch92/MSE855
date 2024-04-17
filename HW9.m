0f5abdf3522a057a0768d7e6a68cf44c82611f6c
Mitch Delemeester <156103459+mistermitch92@users.noreply.github.com>
2024-04-17 11:33:14 -0400

    changed beta to gamma

--- HW9.m
+++ HW9.m
@@ -38,21 +38,21 @@ a_fcc = 0.3571*10^-9; %meters
 
 %data from Barin (converted molar G to volumetric G)
 T_alpha = [298.15 300.00 400.00 500.00 600.00 700.00 800.00 900.00 1000.00 1042.00 1042.00 1100.00 1184.00]';
-T_beta = [1184.00 1200.00 1300.00 1400.00 1500.00 1600.00 1665.00]';
+T_gamma = [1184.00 1200.00 1300.00 1400.00 1500.00 1600.00 1665.00]';
 G_alpha = ([-8.133 -8.184 -11.317 -15.140 -19.559 -24.513 -29.964 -35.891 -42.302 -45.153 -45.153 -49.252 -55.437])'; % kJ/mol
-G_beta = ([-55.437 -56.656 -64.437 -72.486 -80.790 -89.338 -95.020])'; %kJ/mol
+G_gamma = ([-55.437 -56.656 -64.437 -72.486 -80.790 -89.338 -95.020])'; %kJ/mol
 
 %create 2nd order polynomial fits
 fit_alpha = polyfit(T_alpha, G_alpha, 3);
-fit_beta = polyfit(T_beta, G_beta, 4);
+fit_gamma = polyfit(T_gamma, G_gamma, 4);
 
 %evaluate the fits over the temperature range desired
 shift = 30; %temperature shift to make delta G v align with 
 alpha_eval = polyval(fit_alpha, T-shift); %evlaute the fitted functions with the T shift
-beta_eval = polyval(fit_beta, T-shift); %evlaute the fitted functions with the T shift
+gamma_eval = polyval(fit_gamma, T-shift); %evlaute the fitted functions with the T shift
 
 %calculate volumetric delta G
-delta_g_v = (beta_eval-alpha_eval).*1000*1000000*7.8/55.835; %J/m^3
+delta_g_v = (gamma_eval-alpha_eval).*1000*1000000*7.8/55.835; %J/m^3
 
 
 
@@ -75,9 +75,9 @@ Transf_rate = G_dot.*N;
 figure
 scatter(T_alpha-273, G_alpha,'blue')
 hold on
-scatter(T_beta-273, G_beta,'red')
+scatter(T_gamma-273, G_gamma,'red')
 plot(T-273, alpha_eval,'blue')
-plot(T-273, beta_eval,'red')
+plot(T-273, gamma_eval,'red')
 xlabel ("Temperature (C)")
 ylabel("Gibbs Free Energy (kJ/mol)")
 legend("\alpha","\gamma",'fontsize',18)
